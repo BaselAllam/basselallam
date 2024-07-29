bbe4d376a5b46bab177e1f3097ec91822609682d
baselallam <baseljahen@gmail.com>
2024-07-29 10:39:27 +0300

    firstDeply

--- basselallam/views.py
+++ basselallam/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from gallery.models import Gallery
+from portfolio.models import Portfolio
+from reviews.models import Reviews
+from courses.models import Courses
 
 
 def home_view(request):
-    return render(request, 'index.html')
+    data = {}
+    gallery = Gallery.objects.filter(is_archived = False)
+    data['gallery'] = gallery
+    portfolio = Portfolio.objects.filter(is_archived = False)
+    data['portfolio'] = portfolio
+    reviews = Reviews.objects.filter(is_archived = False)
+    data['reviews'] = reviews
+    courses = Courses.objects.filter(is_archived = False)
+    data['courses'] = courses
+    data['count'] = [10, 20, 30, 40, 50]
+    return render(request, 'index.html', context= data)
