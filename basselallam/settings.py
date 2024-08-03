c3cbccbfedb771c54d1e1a23e5ac6e1c5d28beef
baselallam <baseljahen@gmail.com>
2024-08-03 17:13:15 +0300

    addDomainNameToAllowedHost

--- basselallam/settings.py
+++ basselallam/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(x9q7x=e-d+*1ucd3b@x)t-@9%n4csb5!8gfoa-$*%xi3mj&hg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['basselallam.com']
 
 
 # Application definition
